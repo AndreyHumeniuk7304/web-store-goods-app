7a17254c179e972b698963005987cea92116a494
Andrey <andreyhumeniuk99@gmail.com>
2022-07-06 17:19:38 +0300

    sd

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -4,7 +4,6 @@ import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
 import { showModal } from "../../store/modal/actions";
 import { setFavourites } from "../../store/favourites/actions";
-import { getProducts } from "../../../public/images";
 
 const ProductCard = ({
   // handleClickModalAction,
